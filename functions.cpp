160cbcd274900cd7ca2140cef9a283ed4df012dd
aanchal0506 <aanchalladha22@gmail.com>
2025-10-26 09:23:25 +0530

    functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -1,1 +1,21 @@
 
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int add(int a, int b){
+    int z;
+    z=a+b;
+    return (z);
+}
+void add2(int e, int f){
+    int c=e+f;
+    cout<<"sum of two integer:"<<c<<endl;
+}
+int main() {
+    int x,y;
+    x=10;
+    y=20;
+    int sum=add(x,y);
+    cout<<"sum of two integer:"<<sum<<endl;
+    add2(x,y);
+}
