bbfc8ae23cbd1d762003e9c5b7dab5fa8b2f478a
aanchal0506 <aanchalladha22@gmail.com>
2025-10-31 12:06:16 +0530

    sum of n naturl number
    Added iterative sum function to calculate sum of first n integers.

--- sum_recursion.cpp
+++ sum_recursion.cpp
@@ -9,8 +9,16 @@ int sum(int n){
         return sum(n-1)+n;
     }
 } 
-
+int Isum(int n){
+    int s=0;
+    for(int i=1;i<=n;i++){
+        s=s+i;
+    }
+    return s;
+}
 int main() {
   int r=sum(5);
   cout<<r<<endl;
+  int r1=Isum(5);
+  cout<<r1<<endl;
 }
