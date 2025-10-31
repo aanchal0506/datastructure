80537956f0792bcb08ecad22f11b6a308533e538
aanchal0506 <aanchalladha22@gmail.com>
2025-10-31 12:02:43 +0530

    Implement recursive sum function in C++

---
+++ sum_recursion.cpp
@@ -0,0 +1,16 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int sum(int n){
+    if (n==0){
+        return 0;
+        }
+    else{
+        return sum(n-1)+n;
+    }
+} 
+
+int main() {
+  int r=sum(5);
+  cout<<r<<endl;
+}
